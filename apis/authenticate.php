2dcf3f4e9466cd8475a20f594ba7586c25266265
krkunal29 <krkunal29@gmail.com>
2019-10-11 20:17:17 +0530

    # kunal  kapse

--- apis/authenticate.php
+++ apis/authenticate.php
@@ -13,9 +13,27 @@ $sql = "SELECT * FROM  branch_master WHERE username='$usrname' AND password='$pa
         $academicAffected = mysqli_num_rows($jobQuery);
         if ($academicAffected > 0) {
             while ($academicResults = mysqli_fetch_assoc($jobQuery)) {
-                $records = $academicResults;
-                $response = array('Message' => "Login Successfully", "Data" => $records, 'Responsecode' => 200);
-                break;
+                $branchid=$academicResults['branchId'];
+                $sql1 ="SELECT doctorId FROM user_master WHERE branchId=$branchid";
+                $jobQuery1 = mysqli_query($conn,$sql1);
+                if ($jobQuery1 != null)
+                {
+                    $academicAffected1 = mysqli_num_rows($jobQuery1);
+                    if ($academicAffected1 > 0)
+                    {
+                        $academicResults1 = mysqli_fetch_assoc($jobQuery1);
+                        $records['userId']=$academicResults1['doctorId'];
+                        $records['branchId']=$branchid;
+                        $response = array('Message' => "Login Successfully", "Data" => $records, 'Responsecode' => 200);
+                        break;
+                    }
+                    else {
+                      $response = array('Message' => "No user present/ Invalid username or password", "Data" => $records, 'Responsecode' => 401);
+                    }
+                }
+                else {
+                $response = array('Message' => "No user present/ Invalid username or password", "Data" => $records, 'Responsecode' => 401);
+                }
             }
         } else {
             $response = array('Message' => "No user present/ Invalid username or password", "Data" => $records, 'Responsecode' => 401);
