cb9719a85a4582b8c7cb0c87bea6028772464055
vbpawar <vikaspawar3110@gmail.com>
2019-10-22 18:12:11 +0530

    added changes

--- leftsidebar.php
+++ leftsidebar.php
@@ -10,8 +10,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <h1><a class="navbar-brand" href="#"><span class="fa fa-area-chart"></span> Maha<span
-                            class="dashboard_text">MahaVetNet dashboard</span></a></h1>
+                <h5><a class="navbar-brand" href="#">MahaVetNet</a></h5>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="sidebar-menu">
