e5c3014b3357056e6de597a422bcaaaccd8eb41c
vbpawar <vikaspawar3110@gmail.com>
2019-11-09 11:34:31 +0530

    added new

--- jscode/apis.js
+++ jscode/apis.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 //  const url = 'http://praxello.com/ahimsa/apis/';
 const url = 'apis/';
-=======
-const url = 'http://praxello.com/ahimsa/apis/';
- // const url = 'apis/';
->>>>>>> 3fc501fc84c75780cc54da445fef90199ec0a17e
